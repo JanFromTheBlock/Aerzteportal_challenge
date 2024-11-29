9b5925c40ed1c9019c72da59bac541735c31f1d1
JanFromTheBlock <133579648+JanFromTheBlock@users.noreply.github.com>
2024-11-30 00:37:11 +0100

    User models

--- User/models.py
+++ User/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Doctor(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=50)
+    title = models.CharField(max_length=30)
+    speciality = models.CharField(max_length=30)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+class Client(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=50)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    
+    
